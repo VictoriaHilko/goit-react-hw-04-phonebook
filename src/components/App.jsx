de5bb08a7afe14594c8763d1cd36d183d6aa862d
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-10-08 01:46:51 +0300

    fix deliting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { ContactForm } from "./ContactForm/ContactForm";
 import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./Filter/Filter";
 import { Section } from "./Section/Section";
+import { nanoid } from "nanoid";
 
 const CONTACTS_LIST = 'contacts';
 
@@ -15,10 +16,10 @@ export const App = () => {
       return [...data];
     } else {
       return [
-        { name: 'Rosie Simpson', number: '459-12-56' },
-        { name: 'Hermione Kline', number: '443-89-12' },
-        { name: 'Eden Clements', number: '645-17-79' },
-        { name: 'Annie Copeland', number: '227-91-26' },
+        { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+        { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+        { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+        { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
       ];
     }
   });
